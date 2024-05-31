8adcb020e1eabb79fcc07c9eaf63ebe2ad915d6e
Mert Eren <105564577+erenmert-2105@users.noreply.github.com>
2024-05-31 18:07:39 +0300

    Update vengance_dh.py

--- vengance_dh.py
+++ vengance_dh.py
@@ -1,4 +1,5 @@
 import pyscreenshot as ImageGrab
+from PIL import Image 
 import win32api
 import time 
 import mss
@@ -10,48 +11,64 @@ import cv2
 import random
 import pyttsx3
 
+path="C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/"
+method = cv2.TM_CCOEFF_NORMED
 # filter warnings
 warnings.filterwarnings('ignore')
 
 
 def Random():
-    r1 = random.uniform(0.3, 0.1)
+    r1 = random.uniform(0, 0.1)
     return round(r1, 2)
 
 
-def Aura():
-    pyautogui.press('9')
-def Spike():
+def Barbed():
     pyautogui.press('8')
-def Fructure():
-    pyautogui.press('6')
+def Kcommand():
+    pyautogui.press('1')
 def Cleave():
-    pyautogui.press('7')
+    pyautogui.press('5')
+def Chakram():
+    pyautogui.press('9')
+def Cobrashot():
+    pyautogui.press('4')
+def Mark():
+    pyautogui.press('7')    
+def Kshot():
+    pyautogui.press('6')      
+
 
 engine = pyttsx3.init()    
 engine.setProperty('rate', 200)  # Speed of speech
-engine.setProperty('volume', 1)  # Volume (0.0 to 1.0)
+engine.setProperty('volume', 0.5)  # Volume (0.0 to 1.0)
 
 
 
 a=win32api.GetKeyState(192)
 b=a
 
-aura = cv2.imread('Vengance/51,9-35,36_aura.png', cv2.TM_CCOEFF_NORMED)
-spike = cv2.imread('Vengance/10,9-35,36_spike.png', cv2.TM_CCOEFF_NORMED)
-cleave = cv2.imread('Vengance/50,49-35,36_cleave.png', cv2.TM_CCOEFF_NORMED)
-fructure = cv2.imread('Vengance/11,50-35,36_frugcture.png', cv2.TM_CCOEFF_NORMED)
-combat = cv2.imread('Vengance/91,9-35,36_combat.png', cv2.TM_CCOEFF_NORMED)
+
+barbed_count = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Barbed_cound.png"))
+barbed = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Barbed.png"))
+killcommand = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Killcommend.png"))
+cleave = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Aoe_bm.png"))
+combat = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Combat_bm.png"))
+fdeath = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Fdead.png"))
+killshot = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Kshot.png"))
+mark = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Mark.png"))
+cobrashot = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Cobrashot.png"))
+chakram = np.array(Image.open("C:/Users/erenm/OneDrive/Masaüstü/wow_Rotation _Bot/Images/Chakram.png"))    
+
 
 
 state = 2
 state_ =2
 
-engine.say("Press tilda button to start vengance bot")
+engine.say("Press tilda button to start bm bot")
 engine.runAndWait()
 
 while True:
-    time.sleep(0.1)
+    time.sleep(Random())
     a=win32api.GetKeyState(192)
     
     if state==1 and state_==2:
@@ -69,11 +86,16 @@ while True:
         # main code here
         # Define the regions to capture
         regions = [
-            {"left": 51, "top": 9, "width": 35, "height": 36},
-            {"left": 10, "top": 9, "width": 35, "height": 36},
-            {"left": 50, "top": 49, "width": 35, "height": 36},
-            {"left": 11, "top": 50, "width": 35, "height": 36},
-            {"left": 91, "top": 9, "width": 35, "height": 36},
+            {"left": 12, "top": 10, "width": 29, "height": 29},
+            {"left": 45, "top": 10, "width": 29, "height": 29},
+            {"left": 78, "top": 10, "width": 29, "height": 29},
+            {"left": 109, "top": 10, "width": 29, "height": 29},
+            {"left": 143, "top": 12, "width": 29, "height": 29},
+            {"left": 12, "top": 42, "width": 29, "height": 29},
+            {"left": 44, "top": 42, "width": 29, "height": 29},
+            {"left": 75, "top": 42, "width": 29, "height": 29},
+            {"left": 109, "top": 42, "width": 29, "height": 29},
+            {"left": 142, "top": 43, "width": 29, "height": 29}
         ]
         
         captured_images = []
@@ -90,97 +112,84 @@ while True:
                 # Append the image array to the list
                 captured_images.append(img_array)
                 
-        imaura= captured_images[0]
-        imspike= captured_images[1]
-        imcleave=  captured_images[2]
-        imfracture=  captured_images[3]
-        imcombat= captured_images[4]
+        Cbarbed_count= captured_images[0]
+        Cbarbed= captured_images[1]
+        Ckillcommand =  captured_images[2]
+        Ccleave =  captured_images[3]
+        Ccombat=  captured_images[4]
+        Cchakram= captured_images[5]
+        Ccobrashot= captured_images[6]
+        Cmark= captured_images[7]
+        Cfdeath= captured_images[8]
+        Ckshot=  captured_images[9]
+
+        
 
 
-        
-        resultaura = cv2.matchTemplate(imaura, aura, cv2.TM_CCOEFF_NORMED)
-        min_val, max_valaura, min_loc, max_loc = cv2.minMaxLoc(resultaura)
-        
-        resultspike = cv2.matchTemplate(imspike, spike, cv2.TM_CCOEFF_NORMED)
-        min_val, max_valspike, min_loc, max_loc = cv2.minMaxLoc(resultspike)
 
-        resultcleave = cv2.matchTemplate(imcleave, cleave, cv2.TM_CCOEFF_NORMED)
-        min_val, max_valcleave, min_loc, max_loc = cv2.minMaxLoc(resultcleave)
-
-        resultfracture = cv2.matchTemplate(imfracture, fructure, cv2.TM_CCOEFF_NORMED)
-        min_val, max_valfracture, min_loc, max_loc = cv2.minMaxLoc(resultfracture)   
+        Rbarbed_count = cv2.matchTemplate(barbed_count, Cbarbed_count, method)
         
-        resultcombat = cv2.matchTemplate(imcombat, combat, cv2.TM_CCOEFF_NORMED)
-        min_val, max_valcombat, min_loc, max_loc = cv2.minMaxLoc(resultcombat)   
+        Rbarbed = cv2.matchTemplate(barbed, Cbarbed, method)
         
+        Rkillcommand = cv2.matchTemplate(killcommand, Ckillcommand, method)
         
+        Rcleave = cv2.matchTemplate(cleave, Ccleave, method)
         
+        Rcombat = cv2.matchTemplate(combat, Ccombat, method)
         
-        if max_valcombat >0.7:
-            if max_valaura > 0.7:
-                Aura()
-            else:
-                if  max_valspike < 0.3:
-                    Spike()
-                else:
-                    if max_valcleave > 0.7:
-                        Cleave()
-                    else:                  
-                        if max_valfracture > 0.6:
-                            Fructure()
+        Rchakram = cv2.matchTemplate(chakram, Cchakram, method)
+        
+        Rcobrashot = cv2.matchTemplate(cobrashot, Ccobrashot, method)
+        
+        Rmark = cv2.matchTemplate(mark, Cmark, method)
+        
+        Rfdeath = cv2.matchTemplate(fdeath, Cfdeath, method)
+        
+        Rkshot = cv2.matchTemplate(killshot, Ckshot, method)
         
         
-        
-        
-        
+        if Rcombat > 0.65:
+            if Rfdeath < 0.7:
+                if Rmark >0.7:
+                    Mark()
+                    
+                elif Rcleave >0.7:
+                    Cleave()
+                    
+                elif Rbarbed_count > 0.7 and Rbarbed >0.7:
+                    Barbed()   
+                    
+                elif Rkshot > 0.7:
+                    Kshot()                   
+                    
+                elif Rkillcommand >0.7:
+                    Kcommand()
+                    
+                elif Rchakram >0.7:
+                    Chakram()
+                    
+                elif Rcobrashot >0.7:
+                    Cobrashot()
+                    
+                    
+                    
     else:
         state=2
-        
-        
-    
-                      
-      
+                
+                
 
-    
-    
 
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
